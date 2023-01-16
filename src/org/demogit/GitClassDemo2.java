e43d0ae18a8e6d53adc01274505aaf9dc8192e62
John Peter <johnpeterinfotech@gamil.com>
2023-01-16 17:31:23 +0530

    List Added

--- src/org/demogit/GitClassDemo2.java
+++ src/org/demogit/GitClassDemo2.java
@@ -1,11 +1,20 @@
 package org.demogit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GitClassDemo2 {
 	public static void main(String[] args) {
 		
 	
 	int a= 5,b= 6;
 	System.out.println(a+b);
+	
+	List <Integer> li = new ArrayList<>();
+	
+	li.add(a, 10);
+			
+			
 
 }
 }
